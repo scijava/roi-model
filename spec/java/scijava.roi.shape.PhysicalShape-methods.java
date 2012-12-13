4c94daf585f03865919b621b79ffecadf604afe5
Roger Leigh <r.leigh@dundee.ac.uk>
2012-12-13 16:40:01 +0000

    java: Implement shape contains method

---
+++ spec/java/scijava.roi.shape.PhysicalShape-methods.java
@@ -0,0 +1,7 @@
+/**
+ * Determine if a point is contained within the region of interest.
+ *
+ * @param position the point position
+ * @return true if within or false if without
+ */
+public boolean contains(Vertex3D position);
